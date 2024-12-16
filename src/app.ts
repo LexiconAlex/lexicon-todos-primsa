d7015c1a23eb82410e1abc7982ea7cf1ae6df82e
Alexander Sjösten <alexander.sjosten@lexicon.se>
2024-12-16 12:04:00 +0100

    Boiler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-// Write your code here
+import PromptSync from "prompt-sync";
+
+const input = PromptSync();
+
+async function main() {
+  console.log("1. Add todo");
+  console.log("2. Mark todo as completed");
+  console.log("3. List todos");
+  console.log("4. Exit");
+
+  while (true) {
+    const option = input("Select an option: ");
+    switch (option) {
+      case "1":
+        input("Press enter to continue...");
+        break;
+      case "2":
+        console.log("Marking todo...");
+
+        break;
+      case "3":
+        input("Press enter to continue...");
+
+        break;
+      case "4":
+        return;
+      default:
+        console.log("Invalid option selected.");
+        break;
+    }
+  }
+}
+
+main();
