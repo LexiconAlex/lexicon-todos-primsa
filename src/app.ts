c8d25865a144ae2eb176349249bfed9eb9c69424
Alexander Sjösten <alexander.sjosten@lexicon.se>
2024-12-16 12:26:00 +0100

    Update app.ts
    try catch for error handling

--- src/app.ts
+++ src/app.ts
@@ -3,30 +3,33 @@ import PromptSync from "prompt-sync";
 const input = PromptSync();
 
 async function main() {
-  console.log("1. Add todo");
-  console.log("2. Mark todo as completed");
-  console.log("3. List todos");
-  console.log("4. Exit");
-
   while (true) {
-    const option = input("Select an option: ");
-    switch (option) {
-      case "1":
-        input("Press enter to continue...");
-        break;
-      case "2":
-        console.log("Marking todo...");
-
-        break;
-      case "3":
-        input("Press enter to continue...");
-
-        break;
-      case "4":
-        return;
-      default:
-        console.log("Invalid option selected.");
-        break;
+    try {
+      console.log("1. Add todo");
+      console.log("2. Mark todo as completed");
+      console.log("3. List todos");
+      console.log("4. Exit");
+      const option = input("Option: ");
+      switch (option) {
+        case "1":
+          input("Press enter to continue...");
+          break;
+        case "2":
+          input("Press enter to continue...");
+          break;
+        case "3":
+          input("Press enter to continue...");
+          break;
+        case "4":
+          return;
+        default:
+          console.log("Invalid option selected.");
+          break;
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      } else console.log("Unexpected error occurred.");
     }
   }
 }
