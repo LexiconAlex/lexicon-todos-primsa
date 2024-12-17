b46360690259f2827e707bd052945c024c49a117
Alexander Sjösten <alexander.sjosten@lexicon.se>
2024-12-17 14:31:42 +0100

    Update app and schema

--- src/app.ts
+++ src/app.ts
@@ -7,25 +7,29 @@ const prisma = new PrismaClient();
 async function main() {
   while (true) {
     try {
-      console.log("1. Add todo");
-      console.log("2. Mark todo as completed");
-      console.log("3. List todos");
-      console.log("4. Exit");
+      console.log("1. Add user");
+      console.log("2. Add todo");
+      console.log("3. Mark todo as completed");
+      console.log("4. List todos");
+      console.log("5. Exit");
       const option = input("Option: ");
       switch (option) {
         case "1":
+          await addUser();
+          break;
+        case "2":
           await addTodo();
           input("Press enter to continue...");
           break;
-        case "2":
+        case "3":
           await completeTodo();
           input("Press enter to continue...");
           break;
-        case "3":
+        case "4":
           await listTodos();
           input("Press enter to continue...");
           break;
-        case "4":
+        case "5":
           return;
         default:
           console.log("Invalid option selected.");
@@ -41,10 +45,31 @@ async function main() {
 
 main();
 
+async function addUser() {
+  const newUser = input("Enter your name: ");
+  const user = await prisma.user.create({
+    data: { name: newUser },
+  });
+  console.log(user);
+  const answer = await input("Do you want to add a todo(y/n): ");
+  if (answer === "y") {
+    const newTodo = input("Name your todo: ");
+    await prisma.todos.create({
+      data: { title: newTodo, userId: user.id },
+    });
+  }
+}
+
 async function addTodo() {
+  const username = input("Whats your name: ");
+  const user = await prisma.user.findFirstOrThrow({
+    where: {
+      name: username,
+    },
+  });
   const title = input("Name your todo: ");
   await prisma.todos.create({
-    data: { title },
+    data: { title, userId: user.id },
   });
 }
 
